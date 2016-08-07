654f0513342751136f8559360300f7db8aaa7899
Scott LaBounty <slabounty@gmail.com>
2016-08-07 16:52:29 -0700

    Add Simplecov + Tests

--- spec/integration/lisp_spec.rb
+++ spec/integration/lisp_spec.rb
@@ -80,8 +80,20 @@ describe Lisp do
     expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(13)
   end
 
+  it "can add two numbers" do
+    string = "(+ 10 3)"
+    tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
+    expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(13)
+  end
+
   it "can subtract two numbers" do
     string = "(subtract 10 3)"
+    tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
+    expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(7)
+  end
+
+  it "can subtract two numbers" do
+    string = "(- 10 3)"
     tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
     expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(7)
   end
@@ -92,10 +104,100 @@ describe Lisp do
     expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(30)
   end
 
+  it "can multiply two numbers" do
+    string = "(* 10 3)"
+    tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
+    expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(30)
+  end
+
   it "can divide two numbers" do
     string = "(divide 10 2)"
     tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
     expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(5)
+  end
+
+  it "can divide two numbers" do
+    string = "(/ 10 2)"
+    tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
+    expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(5)
+  end
+
+  it "can perform an > operation" do
+    string = "(> 3 10)"
+    tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
+    expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(false)
+  end
+
+  it "can perform an < operation" do
+    string = "(< 3 10)"
+    tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
+    expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(true)
+  end
+
+  it "can perform an >= operation" do
+    string = "(>= 3 3)"
+    tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
+    expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(true)
+  end
+
+  it "can perform an <= operation" do
+    string = "(<= 3 10)"
+    tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
+    expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(true)
+  end
+
+  it "can perform an = operation" do
+    string = "(= 3 10)"
+    tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
+    expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(false)
+  end
+
+  context "when a list is not empty" do
+    it "empty? returns false" do
+      string = "(empty? (list 3 10))"
+      tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
+      expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(false)
+    end
+  end
+
+  context "when a list is empty" do
+    it "empty? returns true" do
+      string = "(empty? (list))"
+      tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
+      expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(true)
+    end
+  end
+
+  context "when a list is not null" do
+    it "null? returns false" do
+      string = "(null? (list 3 10))"
+      tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
+      expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(false)
+    end
+  end
+
+  context "when a list is null" do
+    it "null? returns true" do
+      string = "(null? (list))"
+      tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
+      expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(true)
+    end
+  end
+
+  context "when a list is not equal" do
+    it "equal? returns false" do
+      string = "(equal? (list 3 10) (list 10 3))"
+      tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
+      expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(false)
+    end
+  end
+
+  context "when a list is equal" do
+    it "equal? returns true" do
+      string = "(equal? (list 3 10) (list 3 10))"
+      tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
+      expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq(true)
+    end
   end
 
   it "can perform an if operation" do
@@ -103,5 +205,4 @@ describe Lisp do
     tokenizer = Lisp::Tokenizer.new(StringIO.new(string))
     expect(evaluator.lisp_eval(parser.parse(tokenizer.read_s_expr))).to eq("n")
   end
-
 end
